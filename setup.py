0f5dfd7d632e2db77d172281d8179c2c52d11389
guojiawei0203 <jiawei@suger.io>
2024-12-08 10:23:03 +0800

    change workflow to push py package

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "suger-sdk-python"
-VERSION = "v3.127.466"
+VERSION = "v3.127.469"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
