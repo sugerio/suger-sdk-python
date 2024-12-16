bbea715d6bff3555f36c8959e36551a97312d7f9
guojiawei0203 <jiawei@suger.io>
2024-12-16 17:34:43 +0800

    set version and publish

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "suger-sdk-python"
-VERSION = "1.0.0"
+VERSION = "v3.128.21"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
